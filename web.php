4995c35170dba9ac84db8c43087b89e02ade7cd4
MinumCoklatPanas <akumahappanatuh@gmail.com>
2021-01-14 14:13:14 +0700

    finalized home page and add interaction interface to vizualization

---
+++ web.php
@@ -0,0 +1,33 @@
+<?php
+    // Define your location project directory in htdocs (EX THE FULL PATH: D:\xampp\htdocs\x-kang\simple-routing-with-php)
+    $project_location = "/FP_Production";
+    $project_location = $project_location."/fp-the-hydrocarbons";
+    $me = $project_location;
+
+    // For get URL PATH
+    $request = $_SERVER['REQUEST_URI'];
+    $nama = array("metana","etana","propana","butana");
+    $ix = rand(0,3);
+    $vis = '/FP_Production/fp-the-hydrocarbons/visualize';
+    $hydro_name = "-";
+    if (strpos($request,$vis) !== false and $request != $vis) {
+        $hydro_name = substr($request,45);
+    }
+    // echo $hydro_name;
+
+    if ($hydro_name != "-") {
+        require "views/Visualize/".$hydro_name.".html";
+    }
+
+    switch ($request) {
+        case $me.'/home' :
+            // echo "yo";
+            require "views/index.html";
+            break;
+        case $me.'/visualize' :
+            require "views/Visualize/metana.html";
+            break;
+        case $me.'/contact' :
+            require "views/contact.php";
+            break;
+    }
